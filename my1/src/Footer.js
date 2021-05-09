b16ea5f8abe1106b90f4ce4ee052f249bbe2136d
niveskrishna <66479415+niveskrishna@users.noreply.github.com>
2021-05-09 21:06:59 +0530

    Add files via upload

---
+++ my1/src/Footer.js
@@ -0,0 +1,23 @@
+
+
+const Footer=()=>{
+return (  
+    <footer  className="footer text-center">
+        <div  className="container">
+            <div className="row">
+                <div className="col-lg-4 mb-5 mb-lg-0">
+                    <h4   className="mb-4">LOCATION</h4>
+                    <p  className="pre-wrap lead mb-0">
+                        2215 John Daniel DriveClark, MO 65243
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+);
+
+
+}
+
+
+export default Footer;
